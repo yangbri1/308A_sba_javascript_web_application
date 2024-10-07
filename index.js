11eb75026ed98e9700fc9f3a7cbb768e2fda1c29
brian <yangbri1@msu.edu>
2024-10-07 01:23:25 -0400

    creating pokemon array of images & gifs

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
 
+let tries = 0;
+let limit = 5;
+// initialize an array of Pokemon images
+const img_array = ['images\bulbasaur.png', 'images\charmander.png', 'images\chikorita.png', 'images\cyndaquil.png',
+  'images\mudkip.png', 'images\squirtle.png', 'images\torchic.png', 'images\totodile.png', 'images\treecko.png'];
+
+  // poke_index will be a random number b/t 0-8 (9 pokemons in array)
+let poke_index = Math.floor(Math.random() * img_array.length);
+
+while(tries < limit){
+  // BOM prompts user for an input
+  let input = window.prompt(`Please type in "gif" or "image" to proceed`);
+  // converts input into lowercase & remove any excess whitespaces from front & back
+  input = input.toLowerCase().trim();
+  // if user asks for gif
+  if(input === "gif"){
+
+  }
+  else if(input == "image"){
+    img_array[poke_index]
+  }
+  tries++;
+}
 //create an "async" function so we can use "await" within
 async function pokeData(){
     // try-catch block for error handling
@@ -31,6 +54,11 @@ async function pokeData(){
 
         // change the style from none to block sizing
         imgElement.style.display = "block";
+
+        // invoke poke_guess() function 
+        poke_guess(pokemonName);
+        
+        
         
     }
     // catch any thrown errors
@@ -39,6 +67,27 @@ async function pokeData(){
       // output any caught errors
       console.error(error);
     }
+}
+
+// console.log(pokeData());
+// let img_array = []
+
+
+// helper functions
+
+function poke_guess(pokemon){
+  // if the user input matches the shown Pokemon
+  if(pokemon == "charmander"){
+    // obtain DOM element w/ id of "random"
+    let poke_pic =  document.getElementById("random_img");
+    // console.log(poke_pic);
+    // display BOM window.alert() to user
+    window.alert("Congratulations!! 🥳");
+    // removes any previus filter off of the image
+    poke_pic.style.filter = "none";
+    // poke_pic.classList.remove("blur");
+  }
+
 }
 
 
